efa05be452a9be1d9a6e4abc70be7fa90a3f1dcc
Stephen Owen <sred13@gmail.com>
2017-01-31 21:42:54 -0500

    Create StephenOwen.cs
    Added me!

---
+++ src/Firehose.Web/Authors/StephenOwen.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel.Syndication;
+using System.Web;
+using Firehose.Web.Infrastructure;
+
+namespace Firehose.Web.Authors
+{
+public class StephenOwen : IAmAMicrosoftMVP, IFilterMyBlogPosts
+    {
+        public string FirstName => "Stephen";
+        public string LastName => "Owen";
+        public string ShortBioOrTagLine => "FoxDeploy.SubjectMatter = writes about PowerShell";
+        public string StateOrRegion => "Atlanta";
+        public string EmailAddress => "Stephen@foxdeploy.com";
+        public string TwitterHandle => "FoxDeploy";
+
+        public Uri WebSite => new Uri("http://www.FoxDeploy.com");
+        public IEnumerable<Uri> FeedUris { get { yield return new Uri("https://foxdeploy.com/tag/powershell/feed/"); } }
+
+        DateTime IAmAMicrosoftMVP.FirstAwarded => new DateTime(2015, 1, 1);
+        public string GravatarHash => "3dd39b0d646f3b959b741eb0196c4c21";
+    }
