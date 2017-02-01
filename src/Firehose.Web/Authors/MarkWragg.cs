187affcad956bc40238c376d07966f20c2f93a37
Mark Wragg <mark.wragg@gmail.com>
2017-02-01 09:58:00 +0000

    Adding myself as an author
    I have used the RSS link that is already filtered by tag to my PowerShell posts, so I assumed I didn't need to use your filtering class. 
    
    I also confirm:
    - I have a valid blog & RSS URL
    - Host NO malicious or offensive content on the blog (including photos, swearing, etc.)
    - Blog is active with atleast 3 PowerShell related blog posts in the last 6 months
    - Blog content is my own, and is not someone elses work
    - The content is not sales focused
    - If the blog has mixed content (PowerShell and Personal/Non-PowerShell blogs) a filter has been applied
    - If you delete your blog you will come delete your blog from Planet PowerShell
    - Your blog may be removed at any time if any of these are broken.

---
+++ src/Firehose.Web/Authors/MarkWragg.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel.Syndication;
+using System.Web;
+using Firehose.Web.Infrastructure;
+
+namespace Firehose.Web.Authors
+{
+	public class MarkWragg : IAmACommunityMember
+	{
+		public string FirstName => "Mark";
+		public string LastName => "Wragg";
+		public string ShortBioOrTagLine => "Operations Manager interested in all things Windows, Powershell, AWS, Azure, Chef etc.";
+		public string StateOrRegion => "Hook, England";
+		public string EmailAddress => "mark@wragg.io";
+		public string TwitterHandle => "markwragg";
+		public string GravatarHash => "e8e1b8d0e98f84b10a03f9430334b02f";
+
+		public Uri WebSite => new Uri("https://wragg.io/");
+		public IEnumerable<Uri> FeedUris { get { yield return new Uri("http://wragg.io/tag/powershell/rss"); } }
+	}
+}
