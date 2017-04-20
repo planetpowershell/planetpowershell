e14e80f488ee5853a52d38d89ea4eb3cb03a6f92
MikeShepard <mshepard70@gmail.com>
2017-04-19 21:54:00 -0500

    Fixed GeoPosition error

--- src/Firehose.Web/Authors/MikeShepard.cs
+++ src/Firehose.Web/Authors/MikeShepard.cs
@@ -16,7 +16,7 @@ namespace Firehose.Web.Authors
         public string TwitterHandle => "MikeShepard70";
         public string GitHubHandle => "MikeShepard";
         public string GravatarHash => "45bc55fe65c938b0c65c20101dd73b6c";
-        public GeoPosition Position => new GeoPosition(, );
+        public GeoPosition Position => new GeoPosition(37.1291,-93.5151 );
         public Uri WebSite => new Uri("https://powershellstation.com");
         public IEnumerable<Uri> FeedUris { get { yield return new Uri("https://powershellstation.com/feed"); } }
         public bool Filter(SyndicationItem item)
