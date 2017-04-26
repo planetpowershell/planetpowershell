2ca4eda54cded6d6d8224a655fe5d29ca6ea88ca
Boe Prox <boeprox@gmail.com>
2017-04-25 19:05:55 -0500

    Updated Information

--- src/Firehose.Web/Authors/BoeProx.cs
+++ src/Firehose.Web/Authors/BoeProx.cs
@@ -10,14 +10,14 @@ namespace Firehose.Web.Authors
     public class BoeProx : IAmAMicrosoftMVP
     {
         public string FirstName => "Boe";
-        public string LastName => "Prox`";
+        public string LastName => "Prox";
         public string ShortBioOrTagLine => "SQL DBA, Windows Admin, Security wannabe, who loves PowerShell";
         public string StateOrRegion => "Bellevue, NE";
         public string EmailAddress => "boeprox@gmail.com";
         public string TwitterHandle => "proxb";
         public string GravatarHash => "3bde2094c84fb81661c04ed87f8336b7";
         public string GitHubHandle => "boeprox";
-        public GeoPosition Position => new GeoPosition(41.1544, 95.9146);
+        public GeoPosition Position => new GeoPosition(41.15861, -95.93417);
         public Uri WebSite => new Uri("http://www.learn-powershell.net");
         public IEnumerable<Uri> FeedUris
         {
