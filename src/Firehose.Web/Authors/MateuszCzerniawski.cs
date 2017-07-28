cd170bb27ef1ec2800c2a03b80ad7f21950452ab
Mateusz Czerniawski <mczerniawski@users.noreply.github.com>
2017-07-28 08:43:54 +0200

    Fixed a typo, added ;

--- src/Firehose.Web/Authors/MateuszCzerniawski.cs
+++ src/Firehose.Web/Authors/MateuszCzerniawski.cs
@@ -11,7 +11,7 @@ namespace Firehose.Web.Authors
     {
         public string FirstName => "Mateusz";
         public string LastName => "Czerniawski";
-        public string ShortBioOrTagLine => "PowerShell Enthusiast, WinOps and HyperV"
+        public string ShortBioOrTagLine => "PowerShell Enthusiast, WinOps and HyperV";
         public string StateOrRegion => "Wroclaw, Poland";
         public string EmailAddress => "mczerniawski@arcon.net.pl";
         public string TwitterHandle => "arcontar";
