3987ace1395f13299b0d058fbe8572f32ba279a4
Kunal Udapi <kunaludapi@gmail.com>
2018-06-05 01:25:03 +0530

    Update KunalUdapi.cs

--- src/Firehose.Web/Authors/KunalUdapi.cs
+++ src/Firehose.Web/Authors/KunalUdapi.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.ServiceModel.Syndication;
 using System.Web;
 using Firehose.Web.Infrastructure;
+
 namespace Firehose.Web.Authors
 {
     public class KunalUdapi : IAmACommunityMember, IFilterMyBlogPosts
@@ -19,6 +20,8 @@ namespace Firehose.Web.Authors
         public GeoPosition Position => new GeoPosition(18.5714625,73.8274168);
 
         public Uri WebSite => new Uri("http://vcloud-lab.com");
+        
+        public bool Filter(SyndicationItem item)
         public IEnumerable<Uri> FeedUris { get { yield return new Uri("https://vcloud-lab.com/entries.atom"); } }
         {
             return item.Title.Text.ToLowerInvariant().Contains("powershell") && item.Categories.Any(c => c.Name.ToLowerInvariant().Equals("powershell"));
