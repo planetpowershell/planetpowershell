3db962d78e8b24540279da04970041dfc6b3fed3
Max <maximilian.larum@gmail.com>
2018-07-02 10:03:50 +0200

    Update MaximilianLærum.cs
    Added geo location

--- src/Firehose.Web/Authors/MaximilianLærum.cs
+++ src/Firehose.Web/Authors/MaximilianLærum.cs
@@ -7,6 +7,7 @@ public class MaximilianLærum : IAmACommunityMember
         public string TwitterHandle => "tr4psec";
         public string GravatarHash => "0e45a9759e36d29ac45cf020882cdf5c";
         public string GitHubHandle => "Tr4pSec";
+        public GeoPosition Position => new GeoPosition(59.4172096, 10.4834299);
 
         public Uri WebSite => new Uri("https://get-help.guru/");
         public IEnumerable<Uri> FeedUris { get { yield return new Uri("https://get-help.guru/rss"); } }
