f0a84e2bc196c23fd0765deb7e44f6e7fa931791
Dirk <bremendirk@gmail.com>
2018-07-27 19:54:29 +0100

    Adding DirkBremen

---
+++ src/Firehose.Web/Authors/DirkBremen.cs
@@ -0,0 +1,20 @@
+public class DirkBremen : IAmACommunityMember, IFilterMyBlogPosts
+    {
+        public string FirstName => "Dirk";
+        public string LastName => "Bremen";
+        public string ShortBioOrTagLine => "Automation lover";
+        public string StateOrRegion => "Ireland";
+        public string EmailAddress => "rescueme@planetpowershell.com";
+        public string TwitterHandle => "powershellone";
+        public string GravatarHash => "fc95464388c9f41d7da43a6618fb04bd";
+        public string GitHubHandle => "dbremen";
+        public GeoPosition Position => new GeoPosition(53.338389, -6.258877);
+
+        public Uri WebSite => new Uri("https://powershellone.wordpress.com/");
+        public IEnumerable<Uri> FeedUris { get { yield return new Uri("https://powershellone.wordpress.com/rss"); } }
+        public bool Filter(SyndicationItem item)
+        {
+            // This filters out only the posts that have the "PowerShell" category
+            return item.Categories.Any(c => c.Name.ToLowerInvariant().Equals("powershell"));
+        }
+    }
