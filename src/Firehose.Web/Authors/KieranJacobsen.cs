3e25262ed4331d0433883734bab60288802fb7b4
Kieran Jacobsen <2404128+kjacobsen@users.noreply.github.com>
2018-10-24 17:06:25 +1000

    Remove Xamarin for PowerShell

--- src/Firehose.Web/Authors/KieranJacobsen.cs
+++ src/Firehose.Web/Authors/KieranJacobsen.cs
@@ -28,7 +28,7 @@ namespace Firehose.Web.Authors
 
         public bool Filter(SyndicationItem item)
         {
-            return item.Categories?.Any(c => c.Name.ToLowerInvariant().Equals("xamarin")) ?? false;
+            return item.Categories?.Any(c => c.Name.ToLowerInvariant().Equals("powershell")) ?? false;
         }
 
         public GeoPosition Position => new GeoPosition(-37.816667, 144.966667);
