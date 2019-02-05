aaa91fe629430fc36497ac9b74f070ed48de0416
Bradley Wyatt <bradleywyatt1@gmail.com>
2019-02-05 11:45:05 -0600

    Create BradleyWyatt.cs

---
+++ src/Firehose.Web/Authors/BradleyWyatt.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel.Syndication;
+using System.Web;
+using Firehose.Web.Infrastructure;
+namespace Firehose.Web.Authors
+{
+    public class BradleyWyatt : IAmACommunityMember
+    {
+        public string FirstName => "Bradley";
+        public string LastName => "Wyatt";
+        public string ShortBioOrTagLine => "Finding wayt to do the most work with the least effort possible";
+        public string StateOrRegion => "Chicago, Illinois";
+        public string EmailAddress => "brad@thelazyadministrator.com";
+        public string TwitterHandle => "bwya77";
+        public string GitHubHandle => "bwya77";
+
+        public Uri WebSite => new Uri("https://www.thelazyadministrator.com");
+        public IEnumerable<Uri> FeedUris { get { yield return new Uri("https://www.thelazyadministrator.com/feed/"); } }
+    }
+}
