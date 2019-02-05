b4106eecf4f68e82948db852f9eef175251b7e92
Bradley Wyatt <bradleywyatt1@gmail.com>
2019-02-05 11:57:53 -0600

    Update BradleyWyatt.cs

--- src/Firehose.Web/Authors/BradleyWyatt.cs
+++ src/Firehose.Web/Authors/BradleyWyatt.cs
@@ -1,4 +1,11 @@
-
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel.Syndication;
+using System.Web;
+using Firehose.Web.Infrastructure;
+namespace Firehose.Web.Authors
+{
     public class BradleyWyatt : IAmACommunityMember
     {
         public string FirstName => "Bradley";
@@ -12,4 +19,4 @@
         public Uri WebSite => new Uri("https://www.thelazyadministrator.com");
         public IEnumerable<Uri> FeedUris { get { yield return new Uri("https://www.thelazyadministrator.com/feed/"); } }
     }
-
+} 
