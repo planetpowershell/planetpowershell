d676921e482f7ca4af493e15d455a801e80c9f24
Bradley Wyatt <bradleywyatt1@gmail.com>
2019-02-05 13:37:55 -0600

    Update BradleyWyatt.cs

--- src/Firehose.Web/Authors/BradleyWyatt.cs
+++ src/Firehose.Web/Authors/BradleyWyatt.cs
@@ -12,13 +12,14 @@ namespace Firehose.Web.Authors
         public string FirstName => "Bradley";
         public string LastName => "Wyatt";
         public string ShortBioOrTagLine => "Finding wayt to do the most work with the least effort possible";
-        public string StateOrRegion => "Chicago";
+        public string StateOrRegion => "Chicago, IL, USA";
         public string EmailAddress => "brad@thelazyadministrator.com";
-        public string GravatarHash => "c4eaa00e143c7abb1362dc9a8a48da09";
         public string TwitterHandle => "bwya77";
         public string GitHubHandle => "bwya77";
+        public string GravatarHash => "c4eaa00e143c7abb1362dc9a8a48da09";
+        public GeoPosition Position => new GeoPosition(41.8779810,-87.634656);
 
         public Uri WebSite => new Uri("https://www.thelazyadministrator.com");
         public IEnumerable<Uri> FeedUris { get { yield return new Uri("https://www.thelazyadministrator.com/feed/"); } }
     }
-} 
+}
