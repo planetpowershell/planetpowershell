ff3afa060c48382dc4291c2856b9bd87763d1807
Roberth Strand <roberth@strandkonsult.no>
2019-02-17 19:51:03 +0100

    Added me as author

---
+++ src/Firehose.Web/Authors/RoberthStrand.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel.Syndication;
+using System.Web;
+using Firehose.Web.Infrastructure;
+
+namespace Firehose.Web.Authors
+{
+    public class RoberthStrand : IAmACommunityMember
+    {
+        public string FirstName => "Roberth";
+        public string LastName => "Strand";
+        public string ShortBioOrTagLine => "PowerShell, cloud, security and other fun topics.";
+        public string StateOrRegion => "Tromsø, Norway";
+        public string EmailAddress => "rs@destruktive.one";
+        public string TwitterHandle => "RoberthTweets";
+        public string GravatarHash => "0f8d0c1b68924de4beed584aefd30e20";
+        public string GitHubHandle => "roberthstrand";
+        public GeoPosition Position => new GeoPosition(69.648491, 18.983705);
+        public Uri WebSite => new Uri("https://blog.destruktive.one");
+        public IEnumerable<Uri> FeedUris
+        {
+            get { yield return new Uri("https://blog.destruktive.one/tag/powershell/rss/"); }
+        }
+    }
+}
