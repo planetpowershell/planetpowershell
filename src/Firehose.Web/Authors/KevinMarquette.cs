f35fafbb48978000194877d602554214edf0fb73
Kevin Marquette <kmarquette@loandepot.com>
2019-04-20 19:57:18 -0700

    Adjust Kevin's bio

--- src/Firehose.Web/Authors/KevinMarquette.cs
+++ src/Firehose.Web/Authors/KevinMarquette.cs
@@ -11,7 +11,7 @@ namespace Firehose.Web.Authors
     {
         public string FirstName => "Kevin";
         public string LastName => "Marquette";
-        public string ShortBioOrTagLine => "Sr. DevOps Engineer, 2018 PowerShell Community Hero, Microsoft MVP, and SoCal PowerShell UserGroup Organizer.";
+        public string ShortBioOrTagLine => "Principal DevOps Engineer, Microsoft MVP, 2018 PowerShell Community Hero, and SoCal PowerShell UserGroup Organizer.";
         public string StateOrRegion => "Orange County, USA";
         public string EmailAddress => "kevmar@gmail.com";
         public string TwitterHandle => "kevinmarquette";
