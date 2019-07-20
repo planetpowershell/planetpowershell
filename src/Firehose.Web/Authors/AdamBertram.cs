119c01874202999bd3918a727b1de6e9981ad70d
Adam Bertram <adbertram@gmail.com>
2019-07-20 11:05:40 -0500

    changes to work with my new blog

--- src/Firehose.Web/Authors/AdamBertram.cs
+++ src/Firehose.Web/Authors/AdamBertram.cs
@@ -18,14 +18,14 @@ namespace Firehose.Web.Authors
         public string GravatarHash => "f21b5adac336b3678098de870efcf994";
         public string GitHubHandle => "adbertram";
         public GeoPosition Position => new GeoPosition(37.996239,-87.54378);
-        public Uri WebSite => new Uri("https://www.adamtheautomator.com");
+        public Uri WebSite => new Uri("https://adamtheautomator.com");
         public IEnumerable<Uri> FeedUris
         {
-            get { yield return new Uri("https://www.adamtheautomator.com/feed/"); }
+            get { yield return new Uri("https://adamtheautomator.com/rss/"); }
         }
         public bool Filter(SyndicationItem item)
         {
-            return item.Categories.Any(c => c.Name.ToLowerInvariant().Equals("powershell"));
+            return item.Categories.Any(c => c.Name.ToLowerInvariant().Equals("PowerShell"));
         }
         public string FeedLanguageCode => "en";
     }
