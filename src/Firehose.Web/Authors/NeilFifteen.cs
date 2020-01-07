789006d97fc24883c32042151447d5102103523a
Neil <38805277+digitalfifteen@users.noreply.github.com>
2020-01-07 12:35:32 +0000

    Update NeilFifteen.cs
    I've moved and handles have changed

--- src/Firehose.Web/Authors/NeilFifteen.cs
+++ src/Firehose.Web/Authors/NeilFifteen.cs
@@ -11,13 +11,13 @@ namespace Firehose.Web.Authors
     {
         public string FirstName => "Neil";
         public string LastName => "Fifteen";
-        public string ShortBioOrTagLine => "My adventures found in PowerShell...";
-        public string StateOrRegion => "Cheltenham, England";
+        public string ShortBioOrTagLine => "PowerShell, Whiskey, Buttons and Wires.";
+        public string StateOrRegion => "Ashleworth, England";
         public string EmailAddress => "neil@digitalfifteen.com";
-        public string TwitterHandle => "neil_fifteen";
-        public string GitHubHandle => "neilfifteen";
-        public string GravatarHash => "68ffe557305106c5c399682b5c869cd7";
-        public GeoPosition Position => new GeoPosition(51.897991, -2.071310);
+        public string TwitterHandle => "digitalfifteen";
+        public string GitHubHandle => "digitalfifteen";
+        public string GravatarHash => "0e3cb9812dc19014dfbb000a7953b764";
+        public GeoPosition Position => new GeoPosition(51.930364, -2.274356);
 
         public Uri WebSite => new Uri("https://digitalfifteen.com");
         public IEnumerable<Uri> FeedUris { get { yield return new Uri("https://digitalfifteen.co.uk/feed.xml"); } }
