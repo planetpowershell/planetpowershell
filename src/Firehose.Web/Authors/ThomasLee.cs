7ba2d60fd2d7cf87cb58d282507dc0ac565714cd
Kieran Jacobsen <2404128+kjacobsen@users.noreply.github.com>
2020-01-09 22:21:48 +1100

    fix minor issues

--- src/Firehose.Web/Authors/ThomasLee.cs
+++ src/Firehose.Web/Authors/ThomasLee.cs
@@ -7,18 +7,18 @@ using Firehose.Web.Infrastructure;
 
 namespace Firehose.Web.Authors
 {
-    public class ThomasLEe : IAmAMicrosoftMVP
+    public class ThomasLee : IAmAMicrosoftMVP
     {
         public string FirstName => "Thomas";
         public string LastName => "Lee";
         public string ShortBioOrTagLine => "Just this guy, living in the UK. Grateful Dead fan, and long time PowerShell Fan. MVP 17 times";
         public string StateOrRegion => "Berkshire, UI";
-        public string EmailAddress => 'DoctorDNS@Gmail.Com;
+        public string EmailAddress =>  "DoctorDNS@Gmail.Com";
         public string TwitterHandle => "DoctorDNS";
         public string GravatarHash => "9fac677a9811ddc033b9ec883606031d";
         public string GitHubHandle => "DoctorDNS";
         public GeoPosition Position => new GeoPosition(51.5566737,-0.6941204);
-        public Uri WebSite => new Uri("https://tfl09.blogspot.com.com");
+        public Uri WebSite => new Uri("https://tfl09.blogspot.com");
         public IEnumerable<Uri> FeedUris
         {
             get { yield return new Uri("https://tfl09.blogspot.com/feeds/posts/default/"); }
