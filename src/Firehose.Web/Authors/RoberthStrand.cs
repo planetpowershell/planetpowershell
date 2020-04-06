16a85fb7f36a108b4d9f0bb1a5a823803210a858
Kieran Jacobsen <2404128+kjacobsen@users.noreply.github.com>
2020-04-06 20:09:39 +1000

    Update GravatarHash

--- src/Firehose.Web/Authors/RoberthStrand.cs
+++ src/Firehose.Web/Authors/RoberthStrand.cs
@@ -15,7 +15,7 @@ namespace Firehose.Web.Authors
         public string StateOrRegion => "Tromsø, Norway";
         public string EmailAddress => "me@robstr.dev";
         public string TwitterHandle => "RoberthTweets";
-        public string GravatarHash => "0f8d0c1b68924de4beed584aefd30e20";
+        public string GravatarHash => "b82b95a9133d614b90ef718572959d47";
         public string GitHubHandle => "roberthstrand";
         public GeoPosition Position => new GeoPosition(69.648491, 18.983705);
         public Uri WebSite => new Uri("https://robstr.dev");
