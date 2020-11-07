140bcea0e55041aa5de5dd46215b2f7e36d023b4
itguru4u-be <61526980+itguru4u-be@users.noreply.github.com>
2020-11-07 18:53:49 +0100

    Update MichaelMilitoni.cs

--- src/Firehose.Web/Authors/MichaelMilitoni.cs
+++ src/Firehose.Web/Authors/MichaelMilitoni.cs
@@ -9,7 +9,7 @@ namespace Firehose.Web.Authors
 {
     public class MichaelMilitoni : IAmACommunityMember
     {
-        public string FirstName => "Michael"
+        public string FirstName => "Michael";
         public string LastName => "Militoni";
         public string ShortBioOrTagLine => "An IT pro passionned and specialized in Virtualization";
         public string StateOrRegion => "Brussles, Belgium";
@@ -17,9 +17,10 @@ namespace Firehose.Web.Authors
         public string TwitterHandle => "MichaelMilitoni";
         public string GitHubHandle => "itguru4u-be";
         public string GravatarHash => "";
-        public GeoPosition Position => new GeoPosition(41.887212,-88.302487);
+        public GeoPosition Position => new GeoPosition(50.8503396,4.3517103);
         public Uri WebSite => new Uri("https://www.v-itpassion.be");
         public IEnumerable<Uri> FeedUris { get { yield return new Uri("https://www.v-itpassion.be/category/Powershell/feed/"); } }
         public string FeedLanguageCode => "en";
     }
 }
+
