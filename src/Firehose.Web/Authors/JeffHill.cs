e865a47ed81fda6d658e57e8c99fd9867e95fa2a
sqljeff <59455695+sqljeff@users.noreply.github.com>
2021-06-03 07:22:47 -0500

    create JeffHill.cs

---
+++ src/Firehose.Web/Authors/JeffHill.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel.Syndication;
+using System.Web;
+using Firehose.Web.Infrastructure;
+
+namespace Firehose.Web.Authors
+{
+    public class JeffHill : IFilterMyBlogPosts, IAmACommunityMember
+    {
+        public string FirstName => "Jeff";
+        public string LastName => "Hill";
+        public string ShortBioOrTagLine => "SQL Server DBA with a love for PowerShell.";
+        public string StateOrRegion => "Dallas, TX";
+        public string EmailAddress => "Jeff.Hill@SQLAdm.in";
+        public string TwitterHandle => "sql_jeff";
+        public string GitHubHandle => "sqljeff";
+        public string GravatarHash => "";
+        public GeoPosition Position => new GeoPosition(45.5131063, -122.670492);
+        public Uri WebSite => new Uri("https://sqladm.in/");
+        public IEnumerable<Uri> FeedUris
+        {
+            get { yield return new Uri("https://sqladm.in/posts/index.xml"); }
+        }
+        public bool Filter(SyndicationItem item)
+        {
+            return item.Categories.Where(i => i.Name.Equals("powershell", StringComparison.OrdinalIgnoreCase)).Any();
+        }
+        public string FeedLanguageCode => "en";
+    }
+
+}
