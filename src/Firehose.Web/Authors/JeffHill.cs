b1723dee577d077973a3ab8473717c0a742e0632
sqljeff <59455695+sqljeff@users.noreply.github.com>
2021-06-03 07:33:05 -0500

    Update JeffHill.cs

--- src/Firehose.Web/Authors/JeffHill.cs
+++ src/Firehose.Web/Authors/JeffHill.cs
@@ -16,7 +16,7 @@ namespace Firehose.Web.Authors
         public string EmailAddress => "Jeff.Hill@SQLAdm.in";
         public string TwitterHandle => "sql_jeff";
         public string GitHubHandle => "sqljeff";
-        public string GravatarHash => "";
+        public string GravatarHash => "d83825f965c6ca452e31e9b328561f89";
         public GeoPosition Position => new GeoPosition(45.5131063, -122.670492);
         public Uri WebSite => new Uri("https://sqladm.in/");
         public IEnumerable<Uri> FeedUris
