2591b4c88e70746d0bbc5296b855a605315a32d6
Harm Veenstra <Harm.Veenstra@Gmail.com>
2022-06-08 12:14:21 +0200

    Create HarmVeenstra.cs

---
+++ src/Firehose.Web/Authors/HarmVeenstra.cs
@@ -0,0 +1,15 @@
+  public class HarmVeenstra : IAmACommunityMember
+    {
+        public string FirstName => "Harm";
+        public string LastName => "Veenstra";
+        public string ShortBioOrTagLine => "Cloud Consultant at NEXXT (Https://www.nexxt.one)";
+        public string StateOrRegion => "Overijssel";
+        public string EmailAddress => "harm.veenstra@gmail.com";
+        public string TwitterHandle => "HarmVeenstra";
+        public string GravatarHash => "432c4a0a81ea2b900a6d654452002964";
+        public string GitHubHandle => "HarmVeenstra";
+        public GeoPosition Position => new GeoPosition(52.786708, 6.116538);
+
+        public Uri WebSite => new Uri("https://powershellisfun.com/");
+        public IEnumerable<Uri> FeedUris { get { yield return new Uri("https://powershellisfun.com/feed/"); } }
+    }
