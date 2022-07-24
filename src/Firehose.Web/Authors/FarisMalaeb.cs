1163c71984528964604f054693b41b7a47e979cd
Faris Malaeb <60314774+farismalaeb@users.noreply.github.com>
2022-07-24 12:40:23 +0400

    Update FarisMalaeb.cs

--- src/Firehose.Web/Authors/FarisMalaeb.cs
+++ src/Firehose.Web/Authors/FarisMalaeb.cs
@@ -13,15 +13,18 @@ public class FarisMalaeb : IAmACommunityMember
         public string LastName => "Malaeb";
         public string ShortBioOrTagLine => "System Automation and integration using PowerShell";
         public string StateOrRegion => "Abu Dhabi";
-        public string EmailAddress => "farisnt@gmail.com";
+        public string EmailAddress => "farismalaeb@protonmail.com";
         public string TwitterHandle => "FarisMalaeb";
         public string GravatarHash => "377a1010f17e8d5c5564258012063655";
         public string GitHubHandle => "farismalaeb";
-        public GeoPosition Position => new GeoPosition(24.471011,54.378537);
+        public GeoPosition Position => new GeoPosition(24.471011, 54.378537);
 
         public Uri WebSite => new Uri("https://powershellcenter.com/");
-        public IEnumerable<Uri> FeedUris { get { yield return new Uri("https://www.powershellcenter.com/feed"); } }
+		
+		public IEnumerable<Uri> FeedUris
+        {
+            get { yield return new Uri("https://www.powershellcenter.com/feed"); }
+        }
+        public string FeedLanguageCode => "en";
     }
-
 }
-
