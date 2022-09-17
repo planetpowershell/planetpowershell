40d1d58bfd2ef9720d3cb3ee021d282220413c52
Alain Assaf <alainassaf@outlook.com>
2022-09-17 12:49:43 -0400

    Update AlainAssaf.cs

--- src/Firehose.Web/Authors/AlainAssaf.cs
+++ src/Firehose.Web/Authors/AlainAssaf.cs
@@ -10,15 +10,15 @@ namespace Firehose.Web.Authors
     {
         public string FirstName => "Alain";
         public string LastName => "Alain";
-        public string ShortBioOrTagLine => "Citrix Specialist at SECU - Citrix Technology Advocate - Feedspot Top 50 PowerShell Blogs on the web";
+        public string ShortBioOrTagLine => "EUC Engineer/Architect | Citrix CTA | CCE-V Certified | CUGC Leader | PowerShell Fanatic";
         public string StateOrRegion => "North Carolina, USA";
         public string EmailAddress => "";
         public string TwitterHandle => "alainassaf";
         public string GitHubHandle => "alainassaf";
         public string GravatarHash => "55e3ac1ac8ca9129cc9e138a158b07bb";
         public GeoPosition Position => new GeoPosition(35.7795900, -78.6381790);
-        public Uri WebSite => new Uri("https://alainassaf.github.io/");
-        public IEnumerable<Uri> FeedUris { get { yield return new Uri("https://alainassaf.github.io/feed.xml"); } }
+        public Uri WebSite => new Uri("https://alainassaf.com/");
+        public IEnumerable<Uri> FeedUris { get { yield return new Uri("https://alainassaf.com/feed.xml"); } }
      	public string FeedLanguageCode => "en";
     }
 }
