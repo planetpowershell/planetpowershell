f77a06de44de08e580f115f5423a842c8ff6e465
ehmiiz <elarsson17@gmail.com>
2023-03-08 10:10:25 +0100

    New blog author Emil Larsson

---
+++ src/Firehose.Web/Authors/EmilLarsson.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel.Syndication;
+using System.Web;
+using Firehose.Web.Infrastructure;
+namespace Firehose.Web.Authors
+{
+    public class FerCorrales : IAmACommunityMember
+    {
+        public string FirstName => "Emil";
+        public string LastName => "Larsson";
+        public string Pronouns => "";
+        public string ShortBioOrTagLine => "PowerShell loving hybrid identity architect, blog about PowerShell solutions in Windows Server Core, Active Directory, Azure AD";
+        public string StateOrRegion => "Sweden";
+        public string EmailAddress => "string.Empty";
+        public string TwitterHandle => "ehmiiz";
+        public string MastodonHandle => "@ehmiiz@infosec.exchange";
+        public string GravatarHash => "";
+        public string GitHubHandle => "ehmiiz";
+        public GeoPosition Position => new GeoPosition(60.128161, 18.643501);
+        public Uri WebSite => new Uri("https://ehmiiz.tech/");
+        public IEnumerable<Uri> FeedUris { get { yield return new Uri("https://ehmiiz.tech/index.xml"); } }
+     	public string FeedLanguageCode => "en";
+    }
+	
+}
