61a19e3445f8906205ed7de98454d08528c10db7
ehmiiz <elarsson17@gmail.com>
2023-10-24 10:51:47 +0200

    Fixed relative links in feed EmilLarsson.cs

---
+++ src/Firehose.Web/Authors/EmilLarsson.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel.Syndication;
+using System.Web;
+using Firehose.Web.Infrastructure;
+namespace Firehose.Web.Authors
+{
+    public class EmilLarsson : IAmACommunityMember
+    {
+        public string FirstName => "Emil";enthusiast
+        public string LastName => "Larsson";
+        public string Pronouns => "";
+        public string ShortBioOrTagLine => "Identity, security, automation enthusiast.";
+        public string StateOrRegion => "Sweden";
+        public string EmailAddress => "string.Empty";
+        public string TwitterHandle => "ehmiiz";
+        public string MastodonHandle => "@ehmiiz@infosec.exchange";
+        public string GravatarHash => "0a22f6278e9e9ffdb9f6192ae012ba50";
+        public string GitHubHandle => "ehmiiz";
+        public GeoPosition Position => new GeoPosition(58.114988, 15.031354);
+        public Uri WebSite => new Uri("https://ehmiiz.se/");
+        public IEnumerable<Uri> FeedUris { get { yield return new Uri("https://ehmiiz.se/index.xml"); } }
+     	public string FeedLanguageCode => "en";
+    }
+
+}
