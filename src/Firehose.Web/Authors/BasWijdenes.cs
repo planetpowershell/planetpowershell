0ba737e5a0aaee12e02ddb68c3d27b069aaec134
Bas Wijdenes <baswijdenes@gmail.com>
2023-12-23 15:26:10 +0100

    Update BasWijdenes.cs

--- src/Firehose.Web/Authors/BasWijdenes.cs
+++ src/Firehose.Web/Authors/BasWijdenes.cs
@@ -16,7 +16,6 @@ namespace Firehose.Web.Authors
         public string EmailAddress => String.Empty;
 		public string TwitterHandle => String.Empty;
         public string GitHubHandle => "baswijdenes";
-        public string GravatarHash => "94c48c63e7e63f5e713f7f7a5cdbcac0";
         public Uri WebSite => new Uri("https://baswijdenes.com");
         public GeoPosition Position => new GeoPosition(52.6672892, 4.8682717);
         public IEnumerable<Uri> FeedUris
