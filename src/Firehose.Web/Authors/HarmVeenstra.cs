fdb28add4ab4f0a4ece3d2c5bfa2d4bb3db2b4ed
Harm Veenstra <Harm.Veenstra@Gmail.com>
2024-05-31 21:50:31 +0200

    Adjusted profile for MVP status and Mastodon link

--- src/Firehose.Web/Authors/HarmVeenstra.cs
+++ src/Firehose.Web/Authors/HarmVeenstra.cs
@@ -7,16 +7,16 @@ using Firehose.Web.Infrastructure;
 
 namespace Firehose.Web.Authors
 {
-public class HarmVeenstra : IAmACommunityMember
+public class HarmVeenstra : IAmAMicrosoftMVP
     {
         public string FirstName => "Harm";
         public string LastName => "Veenstra";
         public string Pronouns => "";
-        public string ShortBioOrTagLine => "Cloud Consultant at NEXXT (Https://www.nexxt.one)";
+        public string ShortBioOrTagLine => "Microsoft MVP in PowerShell / Consultant at NEXXT (Https://www.nexxt.one)";
         public string StateOrRegion => "Overijssel";
         public string EmailAddress => "harm.veenstra@gmail.com";
         public string TwitterHandle => "HarmVeenstra";
-        public string MastodonHandle => "";
+        public string MastodonHandle => "@HarmVeenstra@mastodon.social";
         public string GravatarHash => "432c4a0a81ea2b900a6d654452002964";
         public string GitHubHandle => "HarmVeenstra";
         public GeoPosition Position => new GeoPosition(52.786708, 6.116538);
